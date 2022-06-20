2082427d72bbc799e25fc5ff221415745e752d40
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-20 19:08:24 +0200

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,23 @@
+function mySettings(props) {
+  return (
+    <Page>
+      <Section
+        title={<Text bold align="center">Persona amata</Text>}>
+        <TextInput
+          label="Nome: "
+          title="Nome"
+          settingsKey="nome"
+          type="text" 
+        />
+        <TextInput
+          label="Giorno di conoscenza (aaa-mm-gg): "
+          title="Giorno"
+          settingsKey="day"
+          type="text" 
+        />
+      </Section>
+    </Page>
+  );
+}
+
+registerSettingsPage(mySettings);
