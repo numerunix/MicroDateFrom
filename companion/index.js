ca2ae5f8748bcf06eac3885cf847be4c74670950
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-21 15:09:09 +0200

    Update index.js

--- companion/index.js
+++ companion/index.js
@@ -1,9 +1,43 @@
-import { peerSocket } from "messaging";
+import * as messaging from "messaging";
+import { settingsStorage } from "settings";
 
-if (peerSocket.readyState === peerSocket.OPEN) {
-   peerSocket.send("Hello World"); // Should send message to app
+// Message socket opens
+messaging.peerSocket.onopen = () => {
+  console.log("Companion Socket Open");
+  restoreSettings();
+};
+
+// Message socket closes
+messaging.peerSocket.onclose = () => {
+  console.log("Companion Socket Closed");
+};
+
+// A user changes settings
+settingsStorage.onchange = evt => {
+  let data = {
+    key: evt.key,
+    newValue: evt.newValue
+  };
+  sendVal(data);
+};
+
+// Restore any previously saved settings and send to the device
+function restoreSettings() {
+  for (let index = 0; index < settingsStorage.length; index++) {
+    let key = settingsStorage.key(index);
+    if (key) {
+      let data = {
+        key: key,
+        newValue: settingsStorage.getItem(key)
+      };
+      sendVal(data);
+    }
+  }
 }
 
-peerSocket.onerror = (err) => {
-  console.log(`Connection error: ${err.code} - ${err.message}`);
+// Send data to device using Messaging API
+function sendVal(data) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send(data);
+  }
 }
