f9fc7d36b7b1e62e306841adfc93ccb21e06f3a7
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-23 07:37:56 +0200

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -18,6 +18,8 @@ function mySettings(props) {
        <Button list label="Elimina impostazioni" onClick={() => props.settingsStorage.clear()} />
       {<Text bold align="center">Creato da Giulio Sorrentino</Text>}
       {<Text bold align="center">Dedicato a Francesca Milano</Text>}
+      {<Text bold align="center">Questo programma è sotto licenza GPL v3 o, secondo la tua opinione, qualsiasi versione successiva</text>}
+      {<Text bold align="center">https://www.github.com/numerunix/MicroDateFrom</Text>}
       </Section>
     </Page>
   );
