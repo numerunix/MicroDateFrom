bdd3e8631f24e267666d66149d6bb2f99a3d0790
Giulio Sorrentino <49764967+numerunix@users.noreply.github.com>
2022-06-24 02:18:22 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -39,6 +39,7 @@ messaging.peerSocket.onmessage = evt => {
 // Message socket opens
 messaging.peerSocket.onopen = () => {
   console.log("Copyright \u00A92022 Giulio Sorrentino<gsorre84@gmail.com>");
+  console.log("This progam is dedicated to Francesca Milano");
   console.log("This program is inspired by commercial product \"Date From\"");
   console.log("This program is distribuited under GPL. No Warranty is provided.");
   console.log("App Socket Open");
