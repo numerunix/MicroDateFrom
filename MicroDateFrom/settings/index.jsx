5aad436156c27468ccbf10cfc15b00e42dd94709
Giulio Sorrentino <numerone@fastwebnet.it>
2023-12-02 00:25:59 +0100

    Update index.jsx

--- MicroDateFrom/settings/index.jsx
+++ MicroDateFrom/settings/index.jsx
@@ -17,7 +17,7 @@ function mySettings(props) {
         />
        <Button list label="Elimina impostazioni" onClick={() => props.settingsStorage.clear()} />
       {<Text bold align="center">Creato da Giulio Sorrentino</Text>}
-      {<Text bold align="center">Dedicato a Francesca Milano</Text>}        
+      {<Text bold align="center">Dedicato alla barista del Max Bar di Rivisondoli con cui nell'estate del 2021 c'era armonia</Text>}        
       {<Text bold align="center">Questo programma è sotto licenza GPL v3 o, secondo il tuo parere, qualsiasi versione successiva</Text>}
       {<Text bold align="center">https://www.github.com/numerunix/MicroDateFrom</Text>}
       </Section>
